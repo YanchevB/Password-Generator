eba4cbead70519e78516d9739d96becd0e2c7693
Boyan Yanchev <byanchev6@gmail.com>
2025-08-20 14:56:05 +0300

    Configure function to create html

--- scripts/generate-password.js
+++ scripts/generate-password.js
@@ -1,5 +1,12 @@
 import { passwordGenerator } from 'generator.js';
 
 function generateHTML() {
-  
+  const w1 = document.getElementById('w1').value;
+  const w2 = document.getElementById('w2').value;
+  const w3 = document.getElementById('w3').value;
+  const password = passwordGenerator([w1, w2, w3]);
+
+  let passwordHTML = `<div>Your password is: ${password}</div>`;
+  document.querySelector('.js-generated')
+    .innerHTML = passwordHTML;
 }
