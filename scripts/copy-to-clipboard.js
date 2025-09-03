f3b78f6f61ff638df9859853d90dd8d1df83a77d
Boyan Yanchev <byanchev6@gmail.com>
2025-09-03 21:47:53 +0300

    Remove copyText.select()

--- scripts/copy-to-clipboard.js
+++ scripts/copy-to-clipboard.js
@@ -1,6 +1,5 @@
 export function copyToClipboard() {
   let copyText = document.getElementById('myPassword')
-  copyText.select();
   navigator.clipboard.writeText(copyText.value);
   console.log('Text copied');
 }
