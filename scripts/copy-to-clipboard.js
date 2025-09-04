4afa744c474d0f80d66f0ba350678a03f60e5bbf
Boyan Yanchev <byanchev6@gmail.com>
2025-09-04 14:49:18 +0300

    Delete console.log

--- scripts/copy-to-clipboard.js
+++ scripts/copy-to-clipboard.js
@@ -1,5 +1,4 @@
 export function copyToClipboard() {
   let copyText = document.getElementById("myPassword");
   navigator.clipboard.writeText(copyText.innerText);
-  console.log(`${copyText.innerText} copied`);
 }
