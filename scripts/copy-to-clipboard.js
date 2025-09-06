8a2d063bda7d2296274c64fb556cb2b121c5e82b
Boyan Yanchev <byanchev6@gmail.com>
2025-09-06 15:18:32 +0300

    Added inner html change to button when copying onto the clipboard

--- scripts/copy-to-clipboard.js
+++ scripts/copy-to-clipboard.js
@@ -1,4 +1,5 @@
 export function copyToClipboard() {
   let copyText = document.getElementById("myPassword");
   navigator.clipboard.writeText(copyText.innerText);
+  document.querySelector('.js-copy').innerHTML = `Copied!`
 }
